caee3e82fa5d3357d3ce7942c371445ea1b3e069
Joe Paolicelli <joe@joepaolicelli.com>
2017-02-21 02:22:18 -0500

    CORS setup for API

--- autocomplete-api/src/main/java/gwcssd/autocomplete/Api.java
+++ autocomplete-api/src/main/java/gwcssd/autocomplete/Api.java
@@ -8,6 +8,8 @@ import static spark.Spark.*;
  */
 public class Api {
     public static void main(String[] args) {
+        enableCORS("*", "GET,OPTIONS,POST", "*");
+
         get("/autocomplete/:text", (req, res) -> getCompletion(
             req.params(":text")
         ));
@@ -16,5 +18,19 @@ public class Api {
     public static String getCompletion(String givenText) {
         MongoQuerier mq = new MongoQuerier();
         return mq.queryResults(givenText).toJson();
+    }
+
+    // CORS setup.
+    // This method is an initialization method and should be called once.
+    // Original from: https://sparktutorials.github.io/2016/05/01/cors.html
+    private static void enableCORS(
+            final String origin, final String methods, final String headers) {
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Allow-Methods", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            response.type("application/json");
+        });
     }
 }
